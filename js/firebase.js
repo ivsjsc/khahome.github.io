943ca6fe8ffcef914764b75c9e6f6d8c63119874
NGUYỄN MINH TRIẾT <167825527+ivsjsc@users.noreply.github.com>
2025-07-01 18:29:49 +0700

    commit github push and sync

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,7 @@
-// js/firebase.js
+
 
 // Import the functions you need from the SDKs you need
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -19,13 +20,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
-
-// Nếu bạn muốn sử dụng Firestore, bạn sẽ thêm:
-// import { getFirestore } from "firebase/firestore";
-// export const db = getFirestore(app);
-
-// Nếu bạn muốn sử dụng Authentication, bạn sẽ thêm:
-// import { getAuth } from "firebase/auth";
-// export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
